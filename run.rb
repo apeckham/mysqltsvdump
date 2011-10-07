44775c59284cb8bfe01341aa82d0604fca70fe6c
Aaron Peckham <apeckham@local>
2011-10-07 14:42:32 -0700

    show progress while dumping

--- run.rb
+++ run.rb
@@ -1,12 +1,16 @@
+require 'csv'
+
 def system!(*args)
   system(*args) or raise "#{args.inspect} failed"
 end
 
 args = ARGV.join(' ')
 system! "mysqldump -d #{args} >schema.sql"
+system! "mysql #{args} -e'SHOW TABLE STATUS' >status.txt"
 
-tables = `mysql #{args} -N -e'show tables'`.split
-tables.each do |table|
-  puts table
-  system! "mysql #{args} -N -e'SELECT * FROM #{table}' >#{table}.txt"
+pv = `which pv`.chomp
+
+CSV.foreach("status.txt", :headers => true, :col_sep => "\t") do |table|
+  pv_command = "| #{pv} -N #{table["Name"]} -s #{table["Data_length"]}" if !pv.empty?
+  system! "mysql #{args} -N -e'SELECT * FROM #{table["Name"]}' #{pv_command} >#{table["Name"]}.txt"
 end
